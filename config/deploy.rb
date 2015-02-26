cded07925fedb5ee4899bb6532fbf541e22a36ea
Jerrod Long <jerrod@thinkinteractivedesign.com>
2015-02-25 19:10:28 -0500

    update code

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ namespace :deploy do
   desc 'restart application'
   task :restart do
   	on roles(:app), in: :sequence, wait: 5 do
-  		execute :touch, release_path.join('tmp/restart.txt')
+  		execute :touch, release_path.join('/home/websites/henderson/current/tmp/restart.txt')
   	end
   end
 
